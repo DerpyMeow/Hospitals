3b66719e904e072e54c2e3641a041e4f6405072b
DerpyMeow <Gamer@Bens_PC.lan>
2025-03-29 12:43:10 -0400

    -"pills" Fully Completed

---
+++ src/main/java/net/derpymeow/hospitals/procedures/Pill33ateProcedure.java
@@ -0,0 +1,15 @@
+package net.derpymeow.hospitals.procedures;
+
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.effect.MobEffectInstance;
+
+public class Pill33ateProcedure {
+	public static void execute(Entity entity) {
+		if (entity == null)
+			return;
+		if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
+			_entity.addEffect(new MobEffectInstance(MobEffects.REGENERATION, 1200, 3));
+	}
+}
---
+++ src/main/java/net/derpymeow/hospitals/procedures/Pill43ateProcedure.java
@@ -0,0 +1,23 @@
+package net.derpymeow.hospitals.procedures;
+
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.effect.MobEffectInstance;
+
+public class Pill43ateProcedure {
+	public static void execute(Entity entity) {
+		if (entity == null)
+			return;
+		if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
+			_entity.addEffect(new MobEffectInstance(MobEffects.HEAL, 1, 5));
+		if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
+			_entity.addEffect(new MobEffectInstance(MobEffects.LUCK, 1200, 3));
+		if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
+			_entity.addEffect(new MobEffectInstance(MobEffects.REGENERATION, 1200, 3));
+		if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
+			_entity.addEffect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 1200, 3));
+		if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
+			_entity.addEffect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, 1200, 3));
+	}
+}
