ac34a1276db8db15d199a5d469e5cfd7941102e8
Ben <63167383+DerpyMeow@users.noreply.github.com>
2026-02-10 21:23:35 -0500

    v4.0 W.I.P - Removed Unhelpful Tooltips + Moved Workspace Elements

--- src/main/java/net/derpymeow/hospitals/block/BlockOfStainlessSteelBlock.java
+++ src/main/java/net/derpymeow/hospitals/block/BlockOfStainlessSteelBlock.java
@@ -11,13 +11,8 @@ import net.minecraft.world.level.block.DirectionalBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.network.chat.Component;
 import net.minecraft.core.Direction;
 import net.minecraft.core.BlockPos;
-
-import java.util.List;
 
 public class BlockOfStainlessSteelBlock extends Block {
 	public static final DirectionProperty FACING = DirectionalBlock.FACING;
@@ -25,13 +20,6 @@ public class BlockOfStainlessSteelBlock extends Block {
 	public BlockOfStainlessSteelBlock() {
 		super(BlockBehaviour.Properties.of().sound(SoundType.METAL).strength(1.5f, 15f).requiresCorrectToolForDrops());
 		this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
-	}
-
-	@Override
-	public void appendHoverText(ItemStack itemstack, BlockGetter level, List<Component> list, TooltipFlag flag) {
-		super.appendHoverText(itemstack, level, list, flag);
-		list.add(Component.translatable("block.hospitals.block_of_stainless_steel.description_0"));
-		list.add(Component.translatable("block.hospitals.block_of_stainless_steel.description_1"));
 	}
 
 	@Override
--- src/main/java/net/derpymeow/hospitals/block/Labtiles4Block.java
+++ src/main/java/net/derpymeow/hospitals/block/Labtiles4Block.java
@@ -5,23 +5,11 @@ import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.network.chat.Component;
 import net.minecraft.core.BlockPos;
-
-import java.util.List;
 
 public class Labtiles4Block extends Block {
 	public Labtiles4Block() {
 		super(BlockBehaviour.Properties.of().sound(SoundType.STONE).strength(1f, 10f));
-	}
-
-	@Override
-	public void appendHoverText(ItemStack itemstack, BlockGetter level, List<Component> list, TooltipFlag flag) {
-		super.appendHoverText(itemstack, level, list, flag);
-		list.add(Component.translatable("block.hospitals.labtiles_4.description_0"));
-		list.add(Component.translatable("block.hospitals.labtiles_4.description_1"));
 	}
 
 	@Override
